3058556bfbe6366a74a3dd7d919b1a774e5142b7
Joshua Peek <josh@joshpeek.com>
2016-04-27 20:25:05 -0700

    Allow ES6 syntax

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,13 @@
 module.exports = {
+  "parserOptions": {
+    "ecmaVersion": 6
+  },
   "rules": {
     "github/no-implicit-buggy-globals": 0,
     "github/no-sprockets-directives": 0,
   },
   "env": {
+    "es6": true,
     "node": true
   },
   "extends": [
