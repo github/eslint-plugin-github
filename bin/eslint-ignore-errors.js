92a252068547e9f3179ff5df3cb6f813ede82799
Joshua Peek <josh@joshpeek.com>
2016-04-27 20:25:27 -0700

    Add eslint-ignore-errors script

---
+++ bin/eslint-ignore-errors.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+// Disables eslint rules in a JavaScript file with global comments. This is
+// useful when introducing a new rule that causes many failures. The comments
+// can be fixed and removed at while updating the file later.
+//
+// Usage:
+//
+//  eslint-ignore-errors app/assets/javascripts/something.js
+
+const fs = require('fs')
+const execFile = require('child_process').execFile
+
+execFile('eslint', ['--format', 'json', process.argv[2]], (error, stdout) => {
+  JSON.parse(stdout).forEach(result => {
+    const filename = result.filePath
+
+    const ruleIds = new Set()
+    result.messages.forEach(message => ruleIds.add(message.ruleId))
+
+    const js = fs.readFileSync(filename, 'utf8')
+    const comments = Array.from(ruleIds).map(ruleId => `/* eslint ${ruleId}:0 */`).join("\n")
+    fs.writeFileSync(filename, `${comments}\n${js}`, 'utf8')
+  })
+})
