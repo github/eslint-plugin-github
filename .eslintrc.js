ded2da18f09738b91fa3cb6c68b894242af00974
Joshua Peek <josh@joshpeek.com>
2016-05-08 14:41:58 -0700

    Add no-unused-disabled-rules metarule

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   "rules": {
     "github/no-implicit-buggy-globals": 0,
     "github/no-sprockets-directives": 0,
+    "github/no-unused-disabled-rules": 0
   },
   "env": {
     "es6": true,
