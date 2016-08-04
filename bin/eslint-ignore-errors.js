d47ff9ef7590ec47eca0d4f5747fcaea4bebe3a4
Joshua Peek <josh@joshpeek.com>
2016-08-04 13:49:19 -0700

    Only prepend comments if theres any
    Closes #6

--- bin/eslint-ignore-errors.js
+++ bin/eslint-ignore-errors.js
@@ -18,7 +18,9 @@ execFile('eslint', ['--format', 'json', process.argv[2]], (error, stdout) => {
     result.messages.forEach(message => ruleIds.add(message.ruleId))
 
     const js = fs.readFileSync(filename, 'utf8')
-    const comments = Array.from(ruleIds).map(ruleId => `/* eslint-disable ${ruleId} */`).join("\n")
-    fs.writeFileSync(filename, `${comments}\n${js}`, 'utf8')
+    const comments = Array.from(ruleIds).map(ruleId => `/* eslint-disable ${ruleId} */`)
+    if (comments.length) {
+      fs.writeFileSync(filename, `${comments.join("\n")}\n${js}`, 'utf8')
+    }
   })
 })
