2b5cbb2ccf9832d5dd883de1dbf9a18892d11d6d
Joshua Peek <josh@joshpeek.com>
2017-04-18 13:37:55 -0700

    Add async-preventdefault rule

---
+++ tests/async-preventdefault.js
@@ -0,0 +1,27 @@
+var rule = require('../lib/rules/async-preventdefault')
+var RuleTester = require('eslint').RuleTester
+
+var ruleTester = new RuleTester()
+
+ruleTester.run("async-preventdefault", rule, {
+  valid: [
+    {
+      code: "document.addEventListener(function(event) { event.preventDefault() })",
+    },
+    {
+      code: "document.addEventListener(function(event) { event.target })",
+    }
+  ],
+  invalid: [
+    {
+      code: "document.addEventListener(async function(event) { event.preventDefault() })",
+      parserOptions: { ecmaVersion: 2017 },
+      errors: [
+        {
+          message: "event.preventDefault() inside an async function is error prone",
+          type: "CallExpression"
+        }
+      ]
+    }
+  ]
+})
