2722d990d8751ccaeddb9ba388ddc782f256949c
Pat Nakajima <patnakajima@gmail.com>
2017-04-18 21:13:49 -0700

    Use a more specific selector for dataset detection

--- tests/no-dataset.js
+++ tests/no-dataset.js
@@ -6,6 +6,8 @@ var ruleTester = new RuleTester()
 ruleTester.run("no-dataset", rule, {
   valid: [
     { code: "el.getAttribute('data-cool-thing')" },
+    { code: "var dataset = 'this is cool'" },
+    { code: "function dataset() { }" },
   ],
   invalid: [
     {
