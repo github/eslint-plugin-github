d13b22d77f4e877b379b2e9dcb6c381de25570b4
Joshua Peek <josh@joshpeek.com>
2017-04-19 10:52:26 -0700

    Split out flow config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,7 @@ module.exports = {
     "ecmaVersion": 6
   },
   "rules": {
-    "github/no-flow-weak": 0,
-    "github/no-flowfixme": 0,
     "github/no-implicit-buggy-globals": 0,
-    "github/no-noflow": 0,
     "github/no-sprockets-directives": 0,
     "github/no-unused-disabled-rules": 0
   },
