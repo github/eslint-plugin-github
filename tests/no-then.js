4dac4e3b1abe71aa9723ab63ab2fcb12e315592c
Joshua Peek <josh@joshpeek.com>
2017-09-26 16:19:36 -0700

    Catch `catch` as well

--- tests/no-then.js
+++ tests/no-then.js
@@ -8,6 +8,10 @@ ruleTester.run('no-then', rule, {
     {
       code: '(async function() { const data = await read(); console.log(data) })()',
       parserOptions: {ecmaVersion: 2017}
+    },
+    {
+      code: '(async function() { try { await read() } catch(error) { console.error(error) } })()',
+      parserOptions: {ecmaVersion: 2017}
     }
   ],
   invalid: [
@@ -17,6 +21,16 @@ ruleTester.run('no-then', rule, {
       errors: [
         {
           message: 'Prefer async/await to Promise.then()',
+          type: 'Identifier'
+        }
+      ]
+    },
+    {
+      code: '(function() { read().catch(error => console.error(error)) })()',
+      parserOptions: {ecmaVersion: 2017},
+      errors: [
+        {
+          message: 'Prefer async/await to Promise.catch()',
           type: 'Identifier'
         }
       ]
