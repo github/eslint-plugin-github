20e67f314e66ce3b0301a912129033d1d6a0ed57
Mu-An Chiou <me@muanchiou.com>
2019-01-09 13:36:50 -0500

    Add no-d-none rule
    Co-Authored-By: David Graham <dgraham@github.com>

---
+++ tests/no-d-none.js
@@ -0,0 +1,35 @@
+var rule = require('../lib/rules/no-d-none')
+var RuleTester = require('eslint').RuleTester
+
+var ruleTester = new RuleTester()
+
+ruleTester.run('no-d-none', rule, {
+  valid: [
+    {
+      code: 'el.classList.add("dnone")'
+    },
+    {
+      code: 'el.classList.toggle("responsive-d-none")'
+    }
+  ],
+  invalid: [
+    {
+      code: 'el.classList.add("d-none")',
+      errors: [
+        {
+          message: 'Prefer hidden property to d-none class',
+          type: 'CallExpression'
+        }
+      ]
+    },
+    {
+      code: 'el.classList.add("another-class", "d-none")',
+      errors: [
+        {
+          message: 'Prefer hidden property to d-none class',
+          type: 'CallExpression'
+        }
+      ]
+    }
+  ]
+})
