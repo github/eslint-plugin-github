24ea366378a50fc9c1f044412863f09cbd507224
Pablo Núñez <pablonete@github.com>
2021-08-09 10:09:10 +0000

    Add no-only-tests JS linter rule to detect test.only and suite.only

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,13 +8,19 @@ module.exports = {
     node: true
   },
   extends: [require.resolve('./lib/configs/recommended'), 'plugin:eslint-plugin/all'],
-  plugins: ['eslint-plugin'],
+  plugins: ['eslint-plugin', 'no-only-tests'],
   rules: {
     'import/no-commonjs': 'off',
     'filenames/match-regex': 'off',
     'i18n-text/no-en': 'off',
     'eslint-plugin/prefer-placeholders': 'off',
     'eslint-plugin/test-case-shorthand-strings': 'off',
-    'eslint-plugin/require-meta-docs-url': 'off'
+    'eslint-plugin/require-meta-docs-url': 'off',
+    'no-only-tests/no-only-tests': [
+      'error',
+      {
+        'block': ['describe', 'it', 'context', 'test', 'tape', 'fixture', 'serial', 'suite']
+      }
+    ]
   }
 }
