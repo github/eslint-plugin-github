9c94c7278d3382fa758aa7751fdda4be176b73bc
Pablo Núñez <pablonete@github.com>
2021-08-09 12:18:55 +0000

    Add to recommended rules, not to this own project linter rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,19 +8,13 @@ module.exports = {
     node: true
   },
   extends: [require.resolve('./lib/configs/recommended'), 'plugin:eslint-plugin/all'],
-  plugins: ['eslint-plugin', 'no-only-tests'],
+  plugins: ['eslint-plugin'],
   rules: {
     'import/no-commonjs': 'off',
     'filenames/match-regex': 'off',
     'i18n-text/no-en': 'off',
     'eslint-plugin/prefer-placeholders': 'off',
     'eslint-plugin/test-case-shorthand-strings': 'off',
-    'eslint-plugin/require-meta-docs-url': 'off',
-    'no-only-tests/no-only-tests': [
-      'error',
-      {
-        block: ['describe', 'it', 'context', 'test', 'tape', 'fixture', 'serial', 'suite']
-      }
-    ]
+    'eslint-plugin/require-meta-docs-url': 'off'
   }
 }
