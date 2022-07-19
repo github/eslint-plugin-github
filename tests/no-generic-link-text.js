ff98741f98deeda8630591f8ed16ea84ef81b663
Kate Higa <khiga8@github.com>
2022-07-18 17:07:18 -0700

    add new accessibility rule

---
+++ tests/no-generic-link-text.js
@@ -0,0 +1,31 @@
+const rule = require('../lib/rules/no-generic-link-text')
+const RuleTester = require('eslint').RuleTester
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  }
+})
+
+const errorMessage =
+  'Avoid setting generic link text like `Here`, `Click here`, `Read more`. Make sure that your link text is both descriptive and concise.'
+
+ruleTester.run('no-generic-link-text', rule, {
+  valid: [
+    {code: "<a href='#'>GitHub Home</a>;"},
+    {code: "<Box><a href='#'>GitHub Home</a></Box>;"},
+    {code: "<a aria-label='Read more about our project' href='#'>Read more</a>;"},
+    {code: "<a aria-labelledby='someId' href='#'>Read more</a>;"}
+  ],
+  invalid: [
+    {code: '<a>Click here!</a>;', errors: [{message: errorMessage}]},
+    {code: '<a>Learn more.</a>;', errors: [{message: errorMessage}]},
+    {code: "<a aria-label='read more!!!'></a>;", errors: [{message: errorMessage}]},
+    {code: "<a aria-label='click here.'></a>;", errors: [{message: errorMessage}]},
+    {code: "<a aria-label='Here'></a>;", errors: [{message: errorMessage}]}
+  ]
+})
