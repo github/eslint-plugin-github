b35851510864ec03c9836da58f53742eda196030
Kendall Gassner <kendallgassner@github.com>
2023-07-11 16:35:56 +0000

    Add tests that check various component mappings

--- tests/a11y-no-title-attribute.js
+++ tests/a11y-no-title-attribute.js
@@ -20,9 +20,41 @@ ruleTester.run('a11y-no-title-attribute', rule, {
     {code: '<iframe title="an allowed title">GitHub</iframe>'},
     {code: '<span>some information</span>'},
     {code: '<a href="github.com">GitHub</a>'},
+    {
+      code: '<Component title="some title">Submit</Component>',
+      settings: {
+        github: {
+          components: {
+            Component: 'iframe',
+          },
+        },
+      },
+    },
   ],
   invalid: [
     {code: '<a title="some title" href="github.com">GitHub</a>', errors: [{message: errorMessage}]},
     {code: '<span><button title="some title">submit</button></span>', errors: [{message: errorMessage}]},
+    {
+      code: '<Link title="some title">Submit</Link>',
+      errors: [{message: errorMessage}],
+      settings: {
+        github: {
+          components: {
+            Link: 'a',
+          },
+        },
+      },
+    },
+    {
+      code: '<Component as="a" title="some title">Submit</Component>',
+      errors: [{message: errorMessage}],
+      settings: {
+        github: {
+          components: {
+            Component: 'iframe',
+          },
+        },
+      },
+    },
   ],
 })
