c2c7e07e1d59a1ae21d89b6c667bdd3848e01405
Lindsey Wild <35239154+lindseywild@users.noreply.github.com>
2023-07-11 11:21:50 +0000

    add more valid test cases

--- tests/a11y-svg-has-accessible-name.js
+++ tests/a11y-svg-has-accessible-name.js
@@ -12,13 +12,15 @@ const ruleTester = new RuleTester({
 })
 
 const errorMessage =
-  '`<svg>` must have accessible text. Set `aria-label` or `aria-labelledby`, or nest a `<title>` element. However, if the `<svg>` is purely decorative, hide it with `aria-hidden="true"`.'
+  '`<svg>` must have accessible text. Set `aria-label` or `aria-labelledby`, or nest a `<title>` element. However, if the `<svg>` is purely decorative, hide it with `aria-hidden="true" or `role="presentation"`.'
 
 ruleTester.run('a11y-aria-label-is-well-formatted', rule, {
   valid: [
     {code: "<svg height='100' width='100'><title>Circle with a black outline and red fill</title><circle cx='50' cy='50' r='40' stroke='black' stroke-width='3' fill='red'/></svg>"},
     {code: "<svg aria-label='Circle with a black outline and red fill' height='100' width='100'><circle cx='50' cy='50' r='40' stroke='black' stroke-width='3' fill='red'/></svg>"},
     {code: "<svg aria-labelledby='circle_text' height='100' width='100'><circle cx='50' cy='50' r='40' stroke='black' stroke-width='3' fill='red'/></svg>"},
+    {code: "<svg aria-hidden='true' height='100' width='100'><circle cx='50' cy='50' r='40' stroke='black' stroke-width='3' fill='red'/></svg>"},
+    {code: "<svg role='presentation' height='100' width='100'><circle cx='50' cy='50' r='40' stroke='black' stroke-width='3' fill='red'/></svg>"},
   ],
   invalid: [
     {code: "<svg height='100' width='100'><circle cx='50' cy='50' r='40' stroke='black' stroke-width='3' fill='red'/></svg>;", errors: [{message: errorMessage}]},
